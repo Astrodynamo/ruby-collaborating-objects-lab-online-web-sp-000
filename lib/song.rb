13a1ddc27baff345dc68d4d40e6f5550b46111e3
Amanda King <amapickell@gmail.com>
2019-07-13 14:46:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,12 +10,12 @@ class Song
     artist = file.split(" - ")[0]
     new_song = self.new (song)
     new_song.artist_name=(artist)
+    self.artist.add_song(self)
     new_song
   end
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
   end
   
 end
