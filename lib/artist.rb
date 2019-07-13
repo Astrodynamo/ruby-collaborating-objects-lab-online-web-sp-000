9bd68de78a52f42d00977b4a71b527af12978cb9
Amanda King <amapickell@gmail.com>
2019-07-13 15:00:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,6 +25,7 @@ class Artist
   end
   
   def self.find_or_create_by_name (name)
+    self.all.detect {|artist| artist.name == name}
     self.new(name) if !self.all.detect {|artist| artist.name == name}
   end
   
