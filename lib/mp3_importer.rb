a8c4caa203294ee31b5c30fcb09013f401c6dff0
Amanda King <amapickell@gmail.com>
2019-07-13 13:47:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ class MP3Importer
   end
   
   def files
-    Dir["#{@path}/*.mp3"].map {|filename| filename.sub()
+    Dir["#{@path}/*.mp3"].map {|filename| filename.sub("#{@path}/", "")}
   end
   
   def import
