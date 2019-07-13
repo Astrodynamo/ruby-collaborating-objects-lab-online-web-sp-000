12c2bdd47a56e320c258c203736eb972a5b48d18
Amanda King <amapickell@gmail.com>
2019-07-13 14:09:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,12 @@ class Artist
     @songs << song
   end
   
+  def self.all
+    @@all
+  end
+  
   def save
-    @@all << self
+    self.all << self
   end
   
   def self.find_or_create_by_name
