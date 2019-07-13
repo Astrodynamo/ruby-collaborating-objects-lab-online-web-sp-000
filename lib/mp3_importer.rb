7c810f7c7f3177bcf1c311102ad90d0d71a711a6
Amanda King <amapickell@gmail.com>
2019-07-13 13:57:19 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,7 @@ class MP3Importer
   end
   
   def import
-    self.files.each {|song| Song.new_by_filename(song.split(" - ")[1])}
+    self.files.each {|song| Song.new_by_filename(song)}
   end
   
 end
