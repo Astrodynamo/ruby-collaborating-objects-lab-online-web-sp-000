736a17705a4c087018848622e20f310a60b58190
Amanda King <amapickell@gmail.com>
2019-07-13 14:03:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,9 @@ class Song
   end
   
   def self.new_by_filename (file)
-    new_song = self.new ()
-    self.name = file.split(" - ")[1]
+    new_song = self.new (file.split(" - ")[1])
+    new_song.artist = file.split(" - ")[0]
+    new_song
   end
   
 end
